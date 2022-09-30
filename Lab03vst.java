50595dc181085d3455cb4ef00dc72e97b75b3b84
11701 <logan.peek@vtsd.com>
2022-09-30 08:51:43 -0400

    100 point

--- Lab03vst.java
+++ Lab03vst.java
@@ -11,25 +11,23 @@ public class Lab03vst
       System.out.println("Lab03, 80 Point Version\n");
 
 	  int sm = 10000123;
-	  System.out.println("Starting Milli Seconds:" + sm);
+	  System.out.println("Starting Milli Seconds:  " + sm);
 
 	  int h = sm / 3600000;
-	  System.out.println("Hours:" + h);
+	  System.out.println("Hours:                   " + h);
 
 	  int rh = sm % 3600000;
 	  //System.out.println(rh);
 	  int m = rh / 60000;
-	  System.out.println("Minutes:" + m);
+	  System.out.println("Minutes:                 " + m);
 
 	  int rm =rh % 60000;
-	  //System.out.println(rm);
+	 // System.out.println(rm);
 	  int s = rm / 1000;
-	  System.out.println("Seconds:" + s);
+	  System.out.println("Seconds:                 " + s);
 
 	  int rms = rm % 1000;
-	  System.out.println("Milli Seconds:" + rms);
-
-
+	  System.out.println("Milli Seconds:           " + rms);
 
       
 	}
---
+++ MathTest.java
@@ -0,0 +1,17 @@
+
+public class MathTest {
+    public static void main (String args[]){
+        int n1 = 24;
+        int n2 = 7;
+        double n3 = 15;
+        double n4 = 8;
+
+        double sqrtn1 = Math.sqrt(n1);
+        System.out.println(sqrtn1);
+        System.out.println(Math.ceil(sqrtn1));
+
+
+
+
+    }
+}
